6912f238fd53a14d388142f4fcad6c14b3ef2896
Maxim Kondratev <gooooglemax@gmail.com>
2020-11-03 12:30:07 +0300

    Второй пункт

--- lab3_v10/Lotto_v1/lab3_v10.cpp
+++ lab3_v10/Lotto_v1/lab3_v10.cpp
@@ -10,20 +10,19 @@ int main()
     Lotto_card lotto;
     lotto.card_output();
 
-    int temp;
-    cin >> temp;
-    lotto.put_keg(temp);
+    int temp = 0;
 
+    while (temp != -1)
+    {
+        lotto.card_output();
+        
+        cin >> temp;
+        lotto.put_keg(temp);
+    }
+    
+    lotto.check_for_busy_lines();
     lotto.card_output();
 
-    std::vector<int>* array = new std::vector<int>;
-
-    lotto.get_remained_numbers(array);
-
-    for (int i = 0; i < array->size(); i++)
-    {
-        cout << array->at(i) << " ";
-    }
 
     return 0;
 }
